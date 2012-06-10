8b4fb97bd8da2bc75ff2cbb115c00a9a88ab928e
Chakrit Wichian <chakrit@oozou.com>
2012-06-10 16:38:44 +0700

    Remove empty entries from results.

--- node-icu-wordsplit.js
+++ node-icu-wordsplit.js
@@ -7,6 +7,9 @@
 
   var splitWords = require('./build/default/wordsplit.node').splitWords;
 
+  // TODO: Use ICU data for better rejecting useless parts?
+  var REJECT = /^[ \.\!\?\:\;]*$/;
+
   // adds precondition checks (easier to do via JS)
   module.exports = (function(split) {
     return function(locale, text) {
@@ -19,7 +22,18 @@
       text = "" + text;
 
       // should be safe to venture into C land by now
-      return split(locale, text);
+      var rawResults = split(locale, text)
+        , results = [];
+
+      // filter out common chars and empty entries
+      // TODO: Trim from C land? Since we have better ICU support there.
+      for (var i = 0; i < rawResults.length; i++) {
+        if (rawResults[i].match(REJECT)) continue;
+
+        results.push(rawResults[i]);
+      }
+
+      return results;
 
     };
   })(splitWords);
